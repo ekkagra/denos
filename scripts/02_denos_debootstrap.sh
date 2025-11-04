2df6dbaa9ff5a98d242943e5e7b4ae17c1c5447e
ravishankarkm <32084826+ravishankarkm@users.noreply.github.com>
2025-11-04 16:36:52 +0530

    Enhance chroot script with mounts and execution
    Added commands to mount system directories and execute a script in the chroot environment.

--- scripts/02_denos_debootstrap.sh
+++ scripts/02_denos_debootstrap.sh
@@ -18,6 +18,13 @@ $HOME/$LIVE_BOOT/chroot \
 http://ftp.us.debian.org/debian
 
 sudo cp ./03_*.sh $HOME/$LIVE_BOOT/chroot
-sudo cp ./denos_config.txt $HOME/$LIVE_BOOT/chroot
-
+sudo cp ./denos_config.txt $HOME/$LIVE_BOOT/chrootsudo chmod +x $HOME/$LIVE_BOOT/chroot/03_denos_chroot.sh
+sudo mount --bind /dev $HOME/$LIVE_BOOT/chroot/dev
+sudo mount --bind /proc $HOME/$LIVE_BOOT/chroot/proc
+sudo mount --bind /sys $HOME/$LIVE_BOOT/chroot/sys
+sudo chroot $HOME/$LIVE_BOOT/chroot /bin/bash -c "/03_denos_chroot.sh"
+sudo umount $HOME/$LIVE_BOOT/chroot/dev
+sudo umount $HOME/$LIVE_BOOT/chroot/proc
+sudo umount $HOME/$LIVE_BOOT/chroot/sys
+sudo rm -f $HOME/$LIVE_BOOT/chroot/03_denos_chroot.sh
 sudo chroot $HOME/$LIVE_BOOT/chroot
